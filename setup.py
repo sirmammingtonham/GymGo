776e5fd7b6371d8bc66113366e136b54ce5c877f
sirmammingtonham <ethanj217@gmail.com>
2021-12-13 13:49:43 -0500

    add gym_go as a package for pip install

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='gym_go',
     version='0.0.1',
-    install_requires=['gym']  # and other dependencies
+    install_requires=['gym'],  # and other dependencies
+    packages=find_packages(),
 )
